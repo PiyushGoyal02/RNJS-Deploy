09a6c26568fb81b29c06462f1186b2edc8616e83
PiyushGoyal02 <goyalp3542@gmail.com>
2024-06-16 22:14:41 +0530

    Remove Anchor Tag

--- src/Components/ThankYou.js
+++ src/Components/ThankYou.js
@@ -15,7 +15,7 @@ function Thankyou({NextClickHandler}){
                 <p className='Healee-Patient-Journey-Designer'>Thank you For Using Healee</p>
                 <div className='MiroBoardLink-Hading'>
                     {/* <span>Use miro board to design -</span> */}
-                    <a className='AnchorTagMiroBoard' href="https://miro.com/app/board/uXjVKFzLB5o=/?userEmail=goyalp3542@gmail.com&shareBoard=namangarg2025@u.northwestern.edu&track=true&utm_source=notification&utm_medium=email&utm_campaign=request-for-access&utm_content=open-sharing-settings&flow_feature=access_board&flow_type=request">Click here to open the Miro board for designing</a>
+                    {/* <a className='AnchorTagMiroBoard' href="https://miro.com/app/board/uXjVKFzLB5o=/?userEmail=goyalp3542@gmail.com&shareBoard=namangarg2025@u.northwestern.edu&track=true&utm_source=notification&utm_medium=email&utm_campaign=request-for-access&utm_content=open-sharing-settings&flow_feature=access_board&flow_type=request">Click here to open the Miro board for designing</a> */}
                 </div>
                 <button className='NextButtonPages' onClick={HomeClickHandler}> Back To Home</button>
             </div>
